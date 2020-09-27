acc10804285edd4302526453abf8386bc433e303
unknown <benorule@outlook.com>
2020-09-27 19:01:28 +0930

    create heading file

---
+++ src/components/heading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Heading() {
+    return (
+        <div>
+            <h3>Employee Directory</h3>
+            <h6>Use Search Box for quick access</h6>
+        </div>
+    )
+}
