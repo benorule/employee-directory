46f3fc7f2ae88eeb36db3ba91ea1b3cd60ecb882
unknown <benorule@outlook.com>
2020-09-27 19:01:51 +0930

    create container file

---
+++ src/components/container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Heading from './heading.js'
+// import Data from './data.js'
+
+export default function Container() {
+  return (
+      <div>
+          <Heading />
+          {/* <Data /> */}
+      </div>
+  );
+}
